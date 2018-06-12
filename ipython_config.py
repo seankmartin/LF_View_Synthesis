3afdb2bceed377d7266b44de71a96cc10d482eed
Sean Martin <martins7@scss.tcd.ie>
2018-06-12 10:56:48 +0100

    + ipython config

---
+++ ipython_config.py
@@ -0,0 +1,18 @@
+### If you want to auto-save .html and .py versions of your notebook:
+# modified from: https://github.com/ipython/ipython/issues/8009
+import os
+from subprocess import check_call
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts"""
+    if model['type'] != 'notebook':
+        return # only do this for notebooks
+    d, fname = os.path.split(os_path)
+    check_call(['ipython', 'nbconvert', '--to', 'script', fname], cwd=d)
+    check_call(['ipython', 'nbconvert', '--to', 'html', fname], cwd=d)
+
+c = get_config()
+c.FileContentsManager.post_save_hook = post_save
+
+# Run all nodes interactively
+c.InteractiveShell.ast_node_interactivity = "all"
