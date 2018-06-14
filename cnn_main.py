3088a45163e58cf478904ba072311605ce58828e
Sean Martin <martins7@tcd.ie>
2018-06-14 16:40:14 +0100

    + code outline structure

--- cnn_main.py
+++ cnn_main.py
@@ -1,26 +1,38 @@
 import os
 import argparse
 
-import tensorflow as tf
+import h5py
 import numpy as np
-import scipy as sp
-from scipy import io
-from scipy import interpolate
-from scipy import ndimage
-import matplotlib.pyplot as plt
 
-#Set the default base directory
-home = os.path.expanduser('~')
-path_args = [home, 'turing', 'overflow-storage']
-base_dir = os.path.join(*path_args)
+def load_data(data_path):
+    """Returns an array of images from the hdf5 file in data_path"""
+    with h5py.File(data_path, 'r') as h5f:
+        all_images = h5f['images']
+        return all_images
 
-#Modifiable parameters
-parser = argparse.ArgumentParser(
-    description='Process modifiable parameters from command line')
-parser.add_argument('--base_dir', type = str, default = base_dir,
-                    help = 'base directory for the data')
+def main(args):
+    if args.base_dir == "":
+        #Set the default base directory
+        home = os.path.expanduser('~')
+        path_args = [home, 'turing', 'overflow-storage']
+        base_dir = os.path.join(*path_args)
 
-#Load the data from h5 file
-base_dir = args.base_dir
-h5_lf_vis_file_loc = os.path.join(base_dir, 'lfvishead.h5')
-mri_head_vol_file_loc = os.path.join(base_dir, 'lfvis_mri_head_vol.npy')
+    #Load the data
+    h5_lf_vis_file_loc = os.path.join(base_dir, 'lfvishead.h5')
+    images = load_data(h5_lf_vis_file_loc)
+
+    #Perform data augmentation
+
+    #Load the appropriate model with gpu support
+
+    #Perform training and testing [evaluation could go in seperate script]
+        #Store logs and checkpoints of models
+
+if __name__ == '__main__':
+    #Modifiable parameters
+    parser = argparse.ArgumentParser(
+        description='Process modifiable parameters from command line')
+    parser.add_argument('--base_dir', type = str, default = "",
+                        help = 'base directory for the data')
+    args, unparsed = parser.parse_known_args()
+    main(args)
