87d9bd7255fe827abdb761480309a089467b83f8
flabby99 <martins7@msc-linux-sls-035.scss.tcd.ie>
2018-06-15 10:58:37 +0100

    + folder restructure
    + config writer and reader added

--- PythonCode/cnn_main.py
+++ PythonCode/cnn_main.py
@@ -1,16 +1,17 @@
 import os
 import argparse
+import configparser
 
 import h5py
 import numpy as np
 
 def load_data(data_path):
     """Returns an array of images from the hdf5 file in data_path"""
-    with h5py.File(data_path, 'r') as h5f:
-        all_images = h5f['images']
-        return all_images
+    h5f = h5py.File(data_path, 'r')
+    all_images = h5f['images']
+    return all_images
 
-def main(args):
+def main(args, config):
     if args.base_dir == "":
         #Set the default base directory
         home = os.path.expanduser('~')
@@ -29,10 +30,15 @@ def main(args):
         #Store logs and checkpoints of models
 
 if __name__ == '__main__':
-    #Modifiable parameters
+    #Command line modifiable parameters
     parser = argparse.ArgumentParser(
         description='Process modifiable parameters from command line')
     parser.add_argument('--base_dir', type = str, default = "",
                         help = 'base directory for the data')
     args, unparsed = parser.parse_known_args()
-    main(args)
+
+    #Config file modifiable parameters
+    config = configparser.ConfigParser()
+    config.read('main_config.ini')
+    
+    main(args, config)
---
+++ PythonCode/config_writer.py
@@ -0,0 +1,21 @@
+#A script for easily changing the config file
+import configparser
+import os
+
+#Change configurations here
+config = configparser.ConfigParser()
+config['LF_SIZE'] = {'GridSize' : '8',
+                     'ImageWidth' : '256',
+                     'ImageHeight' : '256'}
+
+#Write to the file
+filename = 'main.ini'
+here = os.path.dirname(os.path.abspath(__file__))
+dir_to_make = os.path.join(here, 'config')
+
+if not os.path.exists(dir_to_make):
+    os.makedirs(dir_to_make)
+    
+relative_filepath = os.path.join(here, 'config', filename)
+with open(relative_filepath, 'w') as configfile:
+    config.write(configfile)
