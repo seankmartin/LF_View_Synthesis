7d65c34a9bd17d42e0d50bd332392d11bc192f35
Sean Martin <seankieran.m@hotmail.com>
2018-06-19 10:39:35 +0100

    - Removed extraneous files

--- Inviwo/Inviwo_maths.py
+++
@@ -1,8 +0,0 @@
-from inviwopy.glm import vec3
-
-def cross_product(vec_1: vec3, vec_2: vec3):
-    return vec3(
-        (vec_1.y * vec_2.z) - (vec_1.z * vec_2.y),
-        (vec_1.z * vec_2.x) - (vec_1.x * vec_2.z),
-        (vec_1.x * vec_2.y) - (vec_1.y * vec_2.x)
-    )
