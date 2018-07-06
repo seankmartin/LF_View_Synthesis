198296fc3b3935f4735cf00e82ae95b0e4f27477
Sean Martin <seankieran.m@hotmail.com>
2018-07-06 13:49:00 +0100

    + Data loading and arg parsing

--- PythonCode/cnn_main.py
+++ PythonCode/cnn_main.py
@@ -5,23 +5,12 @@ import configparser
 import h5py
 import numpy as np
 
-def load_data(data_path):
-    """Returns an array of images from the hdf5 file in data_path"""
-    with h5py.File(data_path, 'r') as h5f:
-        all_images = h5f['images']
-        return all_images
+import helpers
 
 def main(args, config):
-    if args.base_dir == "":
-        #Set the default base directory
-        home = os.path.expanduser('~')
-        path_args = [home, 'turing', 'overflow-storage']
-        base_dir = os.path.join(*path_args)
-
     #Load the data
-    h5_lf_vis_file_loc = os.path.join(base_dir, 'lfvishead.h5')
-    images = load_data(h5_lf_vis_file_loc)
-
+    h5_lf_vis_file_loc = os.path.join(config['PATH']['hdf5_dir'],
+                                      config['PATH']['hdf5_name'])
     #Perform data augmentation
 
     #Load the appropriate model with gpu support
@@ -31,14 +20,43 @@ def main(args, config):
 
 if __name__ == '__main__':
     #Command line modifiable parameters
+    #See https://github.com/twtygqyy/pytorch-vdsr/blob/master/main_vdsr.py
+    #For the source of some of these arguments
     parser = argparse.ArgumentParser(
         description='Process modifiable parameters from command line')
     parser.add_argument('--base_dir', type = str, default = "",
                         help = 'base directory for the data')
+    parser.add_argument("--nEpochs", type=int, default=50,
+                        help="Number of epochs to train for")
+    parser.add_argument("--lr", type=float, default=0.1,
+                        help="Learning Rate. Default=0.1")
+    parser.add_argument("--lr_factor", type=float, default=0.1,
+                        help="Factor to reduce learning rate by on plateau")
+    parser.add_argument("--checkpoint", default="", type=str,
+                        help="checkpoint name (default: none)")
+    parser.add_argument("--start-epoch", default=1, type=int,
+                        help="Manual epoch number (useful on restarts)")
+    parser.add_argument("--clip", type=float, default=0.4,
+                        help="Clipping Gradients. Default=0.4")
+    parser.add_argument("--threads", type=int, default=1,
+                        help=" ".join("Number of threads for data loader",
+                                      "to use Default: 1"))
+    parser.add_argument("--momentum", "--m", default=0.9, type=float,
+                        help="Momentum, Default: 0.9")
+    parser.add_argument("--weight-decay", "--wd",
+                        default=1e-4, type=float,
+                        help="Weight decay, Default: 1e-4")
+    parser.add_argument('--pretrained', default='', type=str,
+                        help='name of pretrained model (default: none)')
+    #Any unknown argument will go to unparsed
     args, unparsed = parser.parse_known_args()
 
     #Config file modifiable parameters
     config = configparser.ConfigParser()
     config.read(os.path.join('config','main.ini'))
 
+    print('Program started with the following options')
+    helpers.print_config(config)
+    print('CL arguments')
+    print(args)
     main(args, config)
---
+++ PythonCode/data_loading.py
@@ -0,0 +1,39 @@
+import h5py
+import torch
+import torch.utils.data as data
+
+class DatasetFromHDf5(data.Dataset):
+    def __init__(self, file_path, transform = None):
+        """
+        Keyword arguments:
+        file_path -- the location containing the hdf5 file
+        transform -- an optional transform to apply to the data
+        """
+        super()
+        #Need to close the file on destruction
+        self.hf = h5py.File(file_path, mode = 'r', libver = 'latest')
+        self.depth = hf['depth']
+        self.colour = hf['colour']
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """
+        Return item at index in 0 to len(self)
+        In this case an LF sample
+        Return type is a dictionary of depth and colour
+        """
+        depth = torch.from_numpy(self.depth[index, ...]).float()
+        colour = torch.from_numpy(self.colour[index, ...]).float()
+        sample = {'depth': depth, 'colour': colour}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def __len__(self):
+        """Return the number of samples in the dataset"""
+        return self.depth.attrs['shape'][0]
+
+    def close_h5(self):
+        self.h5.close()
