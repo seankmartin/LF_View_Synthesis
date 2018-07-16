02ea2c0c2e83739f4fd888fd23ed11e797f3f0cc
Sean_M <martins7@tcd.ie>
2018-07-16 13:30:59 +0100

    Added support for a train and val set Patches are extracted during training

--- PythonCode/data_loading.py
+++ PythonCode/data_loading.py
@@ -1,28 +1,98 @@
-import h5py
-
+import numpy.random.uniform as uniform_rand
 import torch
 import torch.utils.data as data
 
-
-class DatasetFromHdf5(data.Dataset):
-    def __init__(self, file_path, transform=None):
+class TrainFromHdf5(data.Dataset):
+    """
+    Creates a training set from a hdf5 file
+    """
+    def __init__(self, hdf_file, patch_size, num_crops, transform=None):
         """
         Keyword arguments:
-        file_path -- the location containing the hdf5 file
+        hdf_file -- the location containing the hdf5 file
+        patch_size -- the size of the patches to extract for training
+        num_crops -- the number of patches to extract for training
         transform -- an optional transform to apply to the data
         """
         super()
         #Need to close the file on destruction
-        self.hdf_file = h5py.File(file_path, mode='r', libver='latest')
-        self.depth = self.hdf_file['depth']
-        self.colour = self.hdf_file['colour']
+        self.group = hdf_file['train']
+        self.depth = self.group['depth']
+        self.colour = self.group['colour']
         self.transform = transform
+        self.patch_size = patch_size
+        self.num_crops = num_crops
 
     def __getitem__(self, index):
         """
         Return item at index in 0 to len(self)
-        In this case an LF sample
-        Return type is a dictionary of depth and colour
+        In this case a set of crops from an lf sample
+        Return type is a dictionary of depth and colour arrays
+        """
+        crop_indexes = self.generate_random_crop()
+        sample = {'depth': [], 'colour': []}
+        for start_h, start_v in crop_indexes:
+            end_h = start_h + self.patch_size
+            end_v = start_v + self.patch_size
+            depth = torch.from_numpy(
+                self.depth[index, :, start_h:end_h, start_v:end_v, :]).float()
+            colour = torch.from_numpy(
+                self.colour[index, :, start_h:end_h, start_v:end_v, :]).float()
+            sample['depth'].append(depth)
+            sample['colour'].append(colour)
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def __len__(self):
+        """Return the number of samples in the dataset"""
+        return self.depth.attrs['shape'][0]
+
+    def generate_random_crop(self):
+        """Return an array of random crops indexes from given light field"""
+        pixel_end = self.depth.attrs['shape'][2]
+        high = pixel_end - self.patch_size
+
+        # An array of indexes to start patch extraction from
+        # Array position [0] would contain patch [0] starting location
+        crop_start_indexes = uniform_rand(0, high, (self.num_crops, 2))
+
+        return crop_start_indexes
+
+class ValFromHdf5(data.Dataset):
+    """
+    Creates a validation set from a hdf5 file
+    """
+    def __init__(self, hdf_file, transform=None):
+        """
+        Keyword arguments:
+        hdf_file -- the location containing the hdf5 file
+        transform -- an optional transform to apply to the data
+        """
+        super()
+        #Need to close the file on destruction
+        self.group = hdf_file['vaimport h5py
+
+
+        self.depth = self.group['import h5py
+
+
+        self.colour = self.group[import h5py
+
+
+        self.transform = transforimport h5py
+
+
+
+    def __getitem__(self, index):import h5py
+
+
+        """
+        Return item at index in 0 to len(self)
+        In this case a set of crops from an lf sample
+        Return type is a dictionary of depth and colour arrays
         """
         depth = torch.from_numpy(self.depth[index, ...]).float()
         colour = torch.from_numpy(self.colour[index, ...]).float()
@@ -36,7 +106,3 @@ class DatasetFromHdf5(data.Dataset):
     def __len__(self):
         """Return the number of samples in the dataset"""
         return self.depth.attrs['shape'][0]
-
-    def close_h5(self):
-        """Close the associated read only hdf5 file"""
-        self.hdf_file.close()
