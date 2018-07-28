e28dc0174cf81a9c51a248e8bd269fc9c2075f35
Sean Martin <seankieran.m@hotmail.com>
2018-07-28 13:33:37 +0100

    Depth differences in Inviwo

--- Inviwo/image_difference_processor.py
+++ Inviwo/image_difference_processor.py
@@ -20,22 +20,35 @@ def process(self):
     im_data = []
     for name in INPORT_LIST:
         im_data.append(self.getInport(name).getData())
-
+    if not (im_data[0].dimensions == 
+            im_data[1].dimensions):
+        print("Operation is incompatible with images of different size")
+        print("Size 1: ", im_data[0].dimensions)
+        print("Size 2: ", im_data[1].dimensions)
+        return -1
+    out_image = Image.clone(im_data[0])
     im_colour = []
     for idx, name in enumerate(INPORT_LIST):
         im_colour.append(im_data[idx].colorLayers[0].data)
     
     out_colour = get_diff_image(im_colour[0], im_colour[1])
-    out_image = Image.clone(im_data[0])
     out_image.colorLayers[0].data = out_colour
+   
+    im_depth = []
+    for idx, name in enumerate(INPORT_LIST):
+        im_depth.append(im_data[idx].depth.data)
+
+    out_depth = get_diff_image(im_depth[0], im_depth[1], np.float32)
+    out_image.depth.data = out_depth
+
     self.getOutport("outport").setData(out_image)
 
 def initializeResources(self):
 	pass
 
-def get_diff_image(im1, im2):
+def get_diff_image(im1, im2, dtype=np.uint8):
     diff = np.subtract(im1.astype(float), im2.astype(float))
-    diff = abs(diff).astype(np.uint8)
+    diff = abs(diff).astype(dtype)
     return diff
 
 # Tell the PythonScriptProcessor about the 'initializeResources' function we want to use
