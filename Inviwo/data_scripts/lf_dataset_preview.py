817b88f20219585c7abfe549a4100bf956e926d9
Sean_M <martins7@tcd.ie>
2018-08-07 14:14:29 +0100

    Updated Inviwo with timings

--- Inviwo/data_scripts/lf_dataset_preview.py
+++ Inviwo/data_scripts/lf_dataset_preview.py
@@ -8,6 +8,7 @@ from lf_camera import LightFieldCamera
 from random_lf import create_random_lf_cameras
 from random_clip import random_clip_lf, restore_clip
 from random_clip import random_plane_clip
+import welford
 
 import inviwopy
 import ivw.utils as inviwo_utils
@@ -30,20 +31,27 @@ def main(pixel_dim, clip, num_random, plane):
                      num_random,
                     (180, 35), 1,
                      interspatial_distance=0.5)
+
+    time_accumulator = (0.0, 0.0, 0.0)
     for lf in random_lfs:
         if clip:
             _, clip_type = random_clip_lf(network, lf)
         elif plane:
             random_plane_clip(network, lf)
-        lf.view_array(cam, save=False)
+        time_taken = lf.view_array(cam, save=False, should_time=True)
+        time_accumulator = welford.update(
+            time_accumulator, time_taken)
         if clip:
             restore_clip(network, clip_type)
+    mean, variance, _ = welford.finalize(time_accumulator)
+    print("Time taken per grid, average {:4f}, std_dev {:4f}".format(
+        mean, math.sqrt(variance)))
 
 if __name__ == '__main__':
     seed(time())
     PIXEL_DIM = 512
     CLIP = False
-    PLANE = True
+    PLANE = False
     NUM_RANDOM_LF_SAMPLES = 2
     
     main(PIXEL_DIM, CLIP, NUM_RANDOM_LF_SAMPLES, PLANE)
