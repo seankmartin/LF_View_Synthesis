08db9ad6c09f21ad4acf4ccf66ff9d0640dd1789
Sean_M <martins7@tcd.ie>
2018-08-10 14:31:49 +0100

    Finally added backwards warping
    Uses skimage and bilinear interpolation
    
    Could port onto GPU if desired

--- PythonCode/image_warping.py
+++ PythonCode/image_warping.py
@@ -1,10 +1,60 @@
 """Image warping based on a disparity"""
 import configparser
 import os
+from enum import Enum
 
 import h5py
 from PIL import Image
 import numpy as np
+from skimage.transform import warp
+import math
+
+import welford
+import evaluate
+
+class WARP_TYPE(Enum):
+    FW = 1
+    SK = 2
+    SLOW = 3
+
+def shift_disp(xy, disp, distance, dtype):
+    #Repeat the elements of the disparity_map to match the distance
+    size_x, size_y = disp.shape[0:2]
+    
+    #Needs to be tranposed to match expected cols, rows
+    repeated = np.repeat(disp.T, 2, -1).reshape((size_x * size_y, 2))
+
+    #Convert to desired dtype
+    result = (repeated * distance).astype(dtype)
+    return xy - result
+
+def sk_warp(
+    ref_view, disparity_map, ref_pos, novel_pos, 
+    dtype=np.float32, blank=0, preserve_range=False):
+    """
+    Uses skimage to perform backward warping:
+    
+    Keyword arguments:
+    ref_view -- colour image data at the reference position
+    disparity_map -- a disparity map at the reference position
+    ref_pos -- the grid co-ordinates of the ref_view
+    novel_pos -- the target grid position for the novel view
+    dtype -- data type to consider disparity as
+    blank -- value to use at positions not seen in reference view
+    preserve_range -- Keep the data in range 0, 255 or convert to 0 1
+    """
+    distance = ref_pos - novel_pos
+    
+    novel_view = warp(
+        image=ref_view, inverse_map=shift_disp, 
+        map_args={
+            "disp": disparity_map, "distance": np.flipud(distance), 
+            "dtype": dtype},
+        cval=blank, preserve_range=preserve_range, order=1
+        )
+    if preserve_range:
+        novel_view = novel_view.astype(np.uint8)
+    return novel_view
 
 def valid_pixel(pixel, img_size):
     """Returns true if the pixel co-ordinate lies inside the image grid"""
@@ -79,6 +129,7 @@ def slow_fw_warp_image(ref_view, disparity_map, ref_pos, novel_pos):
             new_pixel = ((x, y) + res).astype(int)
             if valid_pixel(new_pixel, (size_x, size_y)):
                 novel_view[new_pixel[0], new_pixel[1]] = ref_view[x, y]
+    return novel_view
 
 def save_array_as_image(array, save_location):
     """Saves an array as an image at the save_location using pillow"""
@@ -91,34 +142,101 @@ def get_diff_image(im1, im2):
     diff = abs(diff).astype(np.uint8)
     return diff
 
+def get_diff_image_floatint(im1_float, im2_int):
+    diff = np.subtract(im1_float, im2_int.astype(float) / 255.0)
+    diff = abs(diff).astype(np.uint8)
+    return diff
+
+def get_sub_dir_for_saving(base_dir):
+    """
+    Returns the number of sub directories of base_dir, n, in format
+    base_dir + path_separator + n
+    Where n is padded on the left by zeroes to be of length four
+
+    Example: base_dir is /home/sean/test with two sub directories
+    Output: /home/sean/test/0002
+    """
+    num_sub_dirs = sum(os.path.isdir(os.path.join(base_dir, el))
+                   for el in os.listdir(base_dir))
+
+    sub_dir_to_save_to_name = str(num_sub_dirs)
+    sub_dir_to_save_to_name = sub_dir_to_save_to_name.zfill(4)
+
+    sub_dir_to_save_to = os.path.join(base_dir, sub_dir_to_save_to_name)
+    os.mkdir(sub_dir_to_save_to)
+
+    return sub_dir_to_save_to
+
 def main(config):
     hdf5_path = os.path.join(config['PATH']['output_dir'],
                              config['PATH']['hdf5_name'])
+    warp_type = WARP_TYPE.SK
     with h5py.File(hdf5_path, mode='r', libver='latest') as hdf5_file:
+        grid_size = 64
+        grid_one_way = 8
+        sample_index = grid_size // 2 + (grid_one_way // 2)
         depth_grp = hdf5_file['train']['disparity']
-        SNUM = 0
-        depth_image = depth_grp['images'][SNUM, 27, :, :, 0]
+        SNUM = 2
+        depth_image = depth_grp['images'][SNUM, sample_index, :, :, 0]
 
         #Hardcoded some values for now
         colour_grp = hdf5_file['train']['colour']
-        colour_image = colour_grp['images'][SNUM, 27]
+        colour_image = colour_grp['images'][SNUM, sample_index]
 
         #Can later expand like 0000 if needed
         base_dir = os.path.join(config['PATH']['output_dir'], 'warped')
         get_diff = (config['DEFAULT']['should_get_diff'] == 'True')
+        save_dir = get_sub_dir_for_saving(base_dir)
+        print("Saving images to {}".format(save_dir))
+
+        psnr_accumulator = (0, 0, 0)
+        ssim_accumulator = (0, 0, 0)
         for i in range(8):
             for j in range(8):
-                res = fw_warp_image(colour_image, depth_image,
-                                    np.asarray([3, 3]), np.asarray([i, j]))
-                file_name = 'Colour{}{}.png'.format(i, j)
-                save_location = os.path.join(base_dir, file_name)
+                if warp_type is WARP_TYPE.FW:
+                    res = fw_warp_image(colour_image, depth_image,
+                                        np.asarray([4, 4]), np.asarray([i, j]))
+                elif warp_type is WARP_TYPE.SK:
+                    res = sk_warp(
+                        colour_image, depth_image,
+                        np.asarray([4, 4]), np.asarray([i, j]),
+                        preserve_range=True
+                    )
+                elif warp_type is WARP_TYPE.SLOW:
+                    res = slow_fw_warp_image(
+                        colour_image, depth_image,
+                        np.asarray([4, 4]), np.asarray([i, j])
+                    )
+                file_name = 'Warped_Colour{}{}.png'.format(i, j)
+                save_location = os.path.join(save_dir, file_name)
                 save_array_as_image(res, save_location)
+                idx = i * 8 + j
+                file_name = 'GT_Colour{}{}.png'.format(i, j)
+                save_location = os.path.join(save_dir, file_name)
+                save_array_as_image(
+                    colour_grp['images'][SNUM][idx], save_location)
                 if get_diff:
                     colour = colour_grp['images'][SNUM, i * 8 + j]
                     diff = get_diff_image(colour, res)
+                    #diff = get_diff_image_floatint(res, colour)
                     file_name = 'Diff{}{}.png'.format(i, j)
-                    save_location = os.path.join(base_dir, file_name)
+                    save_location = os.path.join(save_dir, file_name)
                     save_array_as_image(diff, save_location)
+                psnr = evaluate.my_psnr(
+                    res, 
+                    colour_grp['images'][SNUM, i * 8 + j])
+                ssim = evaluate.ssim(
+                    res, 
+                    colour_grp['images'][SNUM, i * 8 + j])
+                psnr_accumulator = welford.update(psnr_accumulator, psnr)
+                ssim_accumulator = welford.update(ssim_accumulator, ssim)
+
+        psnr_mean, psnr_var, _ = welford.finalize(psnr_accumulator)
+        ssim_mean, ssim_var, _ = welford.finalize(ssim_accumulator)
+        print("psnr average {:5f}, stddev {:5f}".format(
+            psnr_mean, math.sqrt(psnr_var)))
+        print("ssim average {:5f}, stddev {:5f}".format(
+            ssim_mean, math.sqrt(ssim_var)))
 
 if __name__ == '__main__':
     CONFIG = configparser.ConfigParser()
