55ee43f63a8a4409de5ab811c21d5f5b54309d3d
Sean_M <martins7@tcd.ie>
2018-08-13 15:55:29 +0100

    Added manual torch seed

--- PythonCode/cnn_main.py
+++ PythonCode/cnn_main.py
@@ -15,6 +15,7 @@ import torch.nn as nn
 import torchvision.utils as vutils
 from tensorboardX import SummaryWriter
 from torch.optim.lr_scheduler import CosineAnnealingLR
+import torch.multiprocessing as mp
 
 import cnn_utils
 from full_model import setup_model
@@ -26,6 +27,13 @@ CONTINUE_MESSAGE = "==> Would you like to continue training?"
 SAVE_MESSAGE = "==> Would you like to save the model?"
 
 def main(args, config, writer):
+    # Used to allow gpu usage during multiprocessing - for manual seed
+    mp.set_start_method('spawn')
+
+    #Used to allow repropucible methods
+    seed = 20
+    torch.manual_seed(seed)
+
     best_loss = math.inf
     best_model, best_epoch = None, None
     cuda = cnn_utils.check_cuda(config)
