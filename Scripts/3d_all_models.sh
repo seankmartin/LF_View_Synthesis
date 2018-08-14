3dea35788abf3b5d1673043ac38965964353a4ab
Sean Martin <martins7@tcd.ie>
2018-08-14 18:49:24 +0100

    Fixed model testing for 3D

--- Scripts/3d_all_models.sh
+++ Scripts/3d_all_models.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 printf "The first variable should be the number of the epochs to test each model for"
 
-#Try the 2D without pretrained
-printf "\nTesting the 2D model with no pretrained data"
-cd ../2D/Scripts
-./run_models.sh "--tag Test2D --n $1 --cfg small.ini"
-cd ../../
-
-#Try the 2D model with pretrained
-printf "\nTesting the 2D model with pretrained data"
-cd Pretrained_2D/Scripts
-./run_models.sh "--tag Test2DPre --pretrained EDSR_x2.pt --n $1 --cfg small.ini"
-cd ../../
-
 #Try the 3D model with no warping
 printf "\nTesting the 3D model without geometrical warping"
 cd Direct_3D/Scripts
