6e0d39860fb6812bb7c0f2b408859f3d824470c9
Sean Martin <martins7@tcd.ie>
2018-08-14 19:35:17 +0100

    Added default freeze value

--- Pretrained_2D/PythonCode/cnn_main.py
+++ Pretrained_2D/PythonCode/cnn_main.py
@@ -256,7 +256,7 @@ if __name__ == '__main__':
     PARSER.add_argument('--res_scale', '--rs', default=1.0, type=float,
                         help="Float to scale residuals by, default 1.0")
     PARSER.add_argument('--frozen', '--f', default=True, type=bool,
-                        help="Should the loaded weights be frozen?")
+                        help="Should the loaded weights be frozen?, default=True")
     #Any unknown argument will go to unparsed
     ARGS, UNPARSED = PARSER.parse_known_args()
     if ARGS.tag is None:
