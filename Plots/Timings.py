8f62c03fdb419a7d84d2c26b21096d9c24678e3c
Sean_M <martins7@tcd.ie>
2018-08-22 21:00:15 +0100

    Added some timings

---
+++ Plots/Timings.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def main():
+    #Overall time mean: 4.175982, stdev: 0.002914
+    #This is in the format mean, stdev, time for only lf grid
+    timing_dict = {
+        'EDSR_remap': (4.175982, 0.002914, 2.91)
+        ,'Direct_render' Overall time mean: 1.134742, stdev: 0.017396
+    }
